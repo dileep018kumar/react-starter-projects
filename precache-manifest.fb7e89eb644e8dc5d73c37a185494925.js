e9c576529bf15ca79727cc4d5671bed1a184904a
dileep018kumar <dileep018kumar@gmail.com>
2021-07-14 19:10:10 +0530

    birthday storer

---
+++ precache-manifest.fb7e89eb644e8dc5d73c37a185494925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f125912cbba1028e6be0ae9e3f24325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f6bf25c5a4c36d4e91",
+    "url": "/static/css/main.03d14b2f.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f6bf25c5a4c36d4e91",
+    "url": "/static/js/main.d7daf999.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
